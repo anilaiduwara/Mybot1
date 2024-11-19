1849794a8bd2fd79a19c3b870217727146e32ac4
Owner by locha <156207819+anilaiduwara@users.noreply.github.com>
2024-11-19 19:40:50 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -2,7 +2,7 @@
 const { cmd } = require('../command')
 const { fetchJson } = require('../lib/functions')
 
-const apilink = 'https://dark-yasiya-api-new.vercel.app' // API LINK ( DO NOT CHANGE THIS!! )
+const apilink = 'https://www.dark-yasiya-api.site' // API LINK ( DO NOT CHANGE THIS!! )
 
 cmd({
     pattern: "song",
